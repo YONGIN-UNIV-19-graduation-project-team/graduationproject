6c39fc394d7bfda6bf54e2aa270ab695aca5ac83
jujonghun <wn1331@gmail.com>
2022-07-31 17:38:08 +0900

    협업 테스트 0731 5:37

--- app/src/main/java/com/nyj/routinemaker/Fragment1.kt
+++ app/src/main/java/com/nyj/routinemaker/Fragment1.kt
@@ -24,7 +24,7 @@ class Fragment1 : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setOnClickListener()
+        setOnClickListener()//협업테스트
     }
 
     fun setOnClickListener() {
