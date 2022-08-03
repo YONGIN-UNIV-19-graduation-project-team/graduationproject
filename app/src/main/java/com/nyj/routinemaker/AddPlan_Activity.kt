ff9fef3366c594c4c59a6f8f9dacf6677b65146b
jujonghun <wn1331@gmail.com>
2022-08-03 18:55:26 +0900

    addplan 파일입출력 커밋푸시 0803 18:55

--- app/src/main/java/com/nyj/routinemaker/AddPlan_Activity.kt
+++ app/src/main/java/com/nyj/routinemaker/AddPlan_Activity.kt
@@ -8,6 +8,8 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_addplan.*
+import java.io.FileWriter
+import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
 //
@@ -16,30 +18,36 @@ class AddPlan_Activity : AppCompatActivity() {
     val formatTime = SimpleDateFormat("HH:MM", Locale.KOREA)
     var plan_Year=""
     var plan_Month=""
-    var plan_day=""
+    var plan_Day=""
     var plan_Hour = ""
     var plan_Min=""
     var plan_Name=""
-//asdf
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_addplan)
-        //화면 나타났을때 year,month,day값 받아와야함
-        //아직미구현//
+
 
 
         var intent = getIntent()
 
         var year = intent.getStringExtra("year").toString()
-        var month = intent.getStringExtra("month").toString()
+        //var month = intent.getStringExtra("month").toString()
         var day = intent.getStringExtra("day").toString()
+        var month = ((intent.getStringExtra("month"))!!.toInt()+1).toString()
+
         //toast로 데이터 넘기기 테스트
         //Toast.makeText(this, "year : "+year, Toast.LENGTH_SHORT ).show()
+
+        plan_Year=year
+        plan_Month=month
+        plan_Day=day
+
 
         if(year != null && year.length > 0 && !year.contains("null") &&
             month != null && month.length > 0 && !month.contains("null") &&
             day != null && day.length > 0 && !day.contains("null")){
-            textview_get_date.setText(year+"년"+month+"월"+day+"일")
+            textview_get_date.setText(year+"년"+(month).toString()+"월"+day+"일")
         }
         else {
             textview_get_date.setText("error")
@@ -59,12 +67,14 @@ class AddPlan_Activity : AppCompatActivity() {
                 Toast.makeText(this, "Date : "+date, Toast.LENGTH_SHORT ).show()
                 textview_get_date.text = date
 
+                plan_Year=selectDate.get(Calendar.YEAR).toString()
+                plan_Month=selectDate.get(Calendar.MONTH).toString()
+                plan_Day=selectDate.get(Calendar.DAY_OF_MONTH).toString()
+
             },getDate.get(Calendar.YEAR),getDate.get(Calendar.MONTH),getDate.get(Calendar.DAY_OF_MONTH))
             datePicker.show()
 
-            plan_Year=Calendar.YEAR.toString()
-            plan_Month=Calendar.MONTH.toString()
-            plan_day=Calendar.DAY_OF_MONTH.toString()
+
         })
 
         //시간 선택 란
@@ -80,8 +90,8 @@ class AddPlan_Activity : AppCompatActivity() {
 
             },getDate.get(Calendar.HOUR),getDate.get(Calendar.MINUTE),true)
             datePicker.show()
-            plan_Hour=Calendar.HOUR.toString()
-            plan_Min=Calendar.MINUTE.toString()
+            plan_Hour=getDate.get(Calendar.HOUR).toString()
+            plan_Min=getDate.get(Calendar.MINUTE).toString()
         })
 
         //저장 버튼 구현
@@ -89,10 +99,24 @@ class AddPlan_Activity : AppCompatActivity() {
             //여기에 파일 입력
             plan_Name=planName.text.toString()//제목가져오기
             Toast.makeText(this
-                , plan_Year+"년"+plan_Month+"월"+plan_day+"일 "+plan_Hour+"시"+plan_Min+"분"
+                , plan_Year+"년"+plan_Month+"월"+plan_Day+"일 "+plan_Hour+"시"+plan_Min+"분"
                 , Toast.LENGTH_SHORT ).show()
+            val path = "/data/data/com.nyj.routinemaker/files/planfile"
+            val writer = FileWriter(path, true)
+            try{
+                writer.write(plan_Year)
+                writer.write(plan_Month)
+                writer.write(plan_Day)
+                writer.write(plan_Hour)
+                writer.write(plan_Min)
+                writer.write("\n")
+            }catch (e: IOException){
+            }finally {
+                writer.close()
+            }//아직미구현
 
-            val intent = Intent(this,MainActivity::class.java)
+
+            //val intent = Intent(this,MainActivity::class.java)
             //startActivity(intent)
         }
     }
