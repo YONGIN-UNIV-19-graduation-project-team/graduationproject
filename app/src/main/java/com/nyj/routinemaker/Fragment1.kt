424a89739907c059e4f660bf73ba618ac543afd7
skduswn <nyj5458@gmail.com>
2022-08-06 17:58:03 +0900

    abc

--- app/src/main/java/com/nyj/routinemaker/Fragment1.kt
+++ app/src/main/java/com/nyj/routinemaker/Fragment1.kt
@@ -1,29 +1,47 @@
 package com.nyj.routinemaker
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
 import android.widget.ArrayAdapter
 import android.widget.Button
+import androidx.constraintlayout.helper.widget.Carousel
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment1.*
 
 class Fragment1 : Fragment() {
+
+    var RoutineList = arrayListOf<RoutineList>(
+        RoutineList(name = "루틴명1", hour = "14", min = "30", dow = "화"),
+        RoutineList(name = "루틴명2", hour = "14", min = "30", dow = "화"),
+        RoutineList(name = "루틴명3", hour = "14", min = "30", dow = "화"),
+        RoutineList(name = "루틴명4", hour = "14", min = "30", dow = "화"),
+        RoutineList(name = "루틴명2", hour = "14", min = "30", dow = "화"),
+        RoutineList(name = "루틴명3", hour = "14", min = "30", dow = "화"),
+        RoutineList(name = "루틴명4", hour = "14", min = "30", dow = "화")
+    )
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        //xml과 연결
+        ////xml과 연결
         val view = inflater.inflate(R.layout.fragment1, container, false)
         return view
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        ////루틴리스트
+        val Adapter = RoutineAdapter(requireContext(), RoutineList)
+        listView.adapter = Adapter
         setOnClickListener()
     }
 
