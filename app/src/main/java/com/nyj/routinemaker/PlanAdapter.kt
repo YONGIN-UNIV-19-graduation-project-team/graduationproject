34187431ee060528c07cede74a2801ca059a69f1
jujonghun <wn1331@gmail.com>
2022-08-08 15:35:48 +0900

    Fragment2.kt에 db연동, 리스트에 띄우기 추가

---
+++ app/src/main/java/com/nyj/routinemaker/PlanAdapter.kt
@@ -0,0 +1,41 @@
+package com.nyj.routinemaker
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+
+class PlanAdapter (val context : Context, val PlanList : ArrayList<Plan>) : BaseAdapter()
+{
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view: View = LayoutInflater.from(context).inflate(R.layout.list_item_plan, null)
+        val name = view.findViewById<TextView>(R.id.plan_name)
+        val data = view.findViewById<TextView>(R.id.plan_data)
+
+        val planList = PlanList[position]
+
+        name.text = planList.name
+        data.text = planList.year+"년 "+planList.month+"월 "+planList.day+"일 "+planList.hour+"시 "+planList.min+"분"
+
+
+
+        return view
+    }
+
+
+    override fun getItem(position: Int): Any {
+        return PlanList[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    override fun getCount(): Int {
+        return PlanList.size
+    }
+
+
+}
