29507655454b7e68eecb5175f677a8dd064f4022
jujonghun <wn1331@gmail.com>
2022-08-09 19:13:59 +0900

    위젯 구현 완료. 8/9 7:13 Plan_DAO.kt searchday 메소드 수정해야함. 위젯이 업데이트 되지 않는 오류 있음

--- app/src/main/java/com/nyj/routinemaker/WidgetProvider.kt
+++ app/src/main/java/com/nyj/routinemaker/WidgetProvider.kt
@@ -3,22 +3,59 @@ package com.nyj.routinemaker
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.os.Build
 import android.widget.RemoteViews
+import androidx.annotation.RequiresApi
+import androidx.room.Room
+import java.time.LocalDate
+
 
 class WidgetProvider :AppWidgetProvider(){
     private val MY_ACTION = "android.action.MY_ACTION"
 
+    var year=""
+    var month = ""
+    var day=""
+    var hour = ""
+    var min = ""
+
+    var Planlist = arrayListOf<Plan>(
+        Plan(0L,"더미","2022","11",
+            "12","0","0")
+    )
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun addviews(context: Context?):RemoteViews{
         val views = RemoteViews(context?.packageName,R.layout.widget)
+        val viewname = StringBuilder()
+
+
+        //db연동, 안될수도있음
+        val db = Room.databaseBuilder(
+            context!!.applicationContext,AppDatabase::class.java,"database"
+        ).allowMainThreadQueries().build()
+        year=LocalDate.now().year.toString()
+        month=LocalDate.now().month.toString()
+        day=LocalDate.now().dayOfMonth.toString()
+        Planlist = db.plan_DAO().searchday(day).toTypedArray().toCollection(arrayListOf<Plan>())
+        db.close()
+        Planlist.forEach{Plan-> viewname.append(Plan.name).append(" ")}
+        println(viewname)
+        views.setTextViewText(R.id.todays_plan,viewname.toString())
+
         return views
+
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onUpdate(
         context: Context?,
         appWidgetManager: AppWidgetManager?,
         appWidgetIds: IntArray?
     ) {
         super.onUpdate(context, appWidgetManager, appWidgetIds)
+
         appWidgetIds?.forEach { appWidgetId->
             val views:RemoteViews = addviews(context)
             appWidgetManager?.updateAppWidget(appWidgetId,views)
